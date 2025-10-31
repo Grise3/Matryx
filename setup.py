1a9cac141c95ac37f667b121bb74c25ea0ad563a
Grise <148749571+Grise3@users.noreply.github.com>
2025-10-31 11:59:46 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="matryx",
     version="0.1.0",
+    author="Grise",
+    description="A Python library for interacting with the Matrix API",
+    url="https://github.com/Grise3/Matryx",
     packages=find_packages(),
     install_requires=[
         'aiohttp>=3.8.0',
         'python-dotenv>=0.19.0',
     ],
+    extras_require={
+        'images': ['Pillow>=8.0.0'],
+        'datetime': ['pytz>=2021.1'],
+    },
     python_requires='>=3.7',
+    project_urls={
+        'Bug Reports': 'https://github.com/Grise3/Matryx/issues',
+        'Source': 'https://github.com/Grise3/Matryx',
+    },
 )
