e755a7357ea745b35beb19e1faedee662f08292f
Grise <148749571+Grise3@users.noreply.github.com>
2025-11-01 10:28:49 +0000

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="matryx",
-    version="0.1.0",
+    version="0.2.0",
     author="Grise",
     description="A Python library for interacting with the Matrix API",
     url="https://github.com/Grise3/Matryx",
